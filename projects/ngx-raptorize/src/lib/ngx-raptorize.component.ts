c076678d3fc75537fc36fc6267de929f1862528e
ThibaudAV <thibaud.avenier@gmail.com>
2019-10-27 10:51:50 +0100

    feat: add raptor sound

--- projects/ngx-raptorize/src/lib/ngx-raptorize.component.ts
+++ projects/ngx-raptorize/src/lib/ngx-raptorize.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { raptorBase64 } from '../assets/raptor-base64';
+import { raptorSoundMP3Base64 } from '../assets/raptor-sound-mp3-base64';
 
 @Component({
   selector: 'ngx-raptorize',
@@ -38,6 +39,7 @@ import { raptorBase64 } from '../assets/raptor-base64';
 })
 export class NgxRaptorizeComponent {
   raptorBase64 = raptorBase64;
+  raptorSoundMP3Base64 = raptorSoundMP3Base64;
 
   @ViewChild('raptor', { static: false })
   raptor: ElementRef;
@@ -46,8 +48,16 @@ export class NgxRaptorizeComponent {
 
   run() {
     this.renderer.addClass(this.raptor.nativeElement, 'show');
+    this.playAudio();
     setTimeout(() => {
       this.renderer.removeClass(this.raptor.nativeElement, 'show');
     }, 2500);
+  }
+
+  private playAudio() {
+    const audio = new Audio();
+    audio.src = raptorSoundMP3Base64;
+    audio.load();
+    audio.play();
   }
 }
